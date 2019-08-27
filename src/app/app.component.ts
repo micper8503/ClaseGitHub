d0ca51666ee0377d610648420152b938f1e5a6d3
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2019-08-26 20:22:49 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.styl']
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'Calculadora-Michael Perez';
+  
+  deshabilitado = true;
+  numero1 = 0;
+  numero2 = 0;
+  valor1:number;
+  valor2:number;
+ 
+
+  constructor(){
+  setTimeout(() => {
+  this.deshabilitado = false;
+  }, 3000)
+  }
+
+  
 }
